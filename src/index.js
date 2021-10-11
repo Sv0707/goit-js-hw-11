2335fbe19e13266a08ce7354d44de057d012593a
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-11 17:34:12 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,50 +1,57 @@
 import './css/styles.css';
 // Описан в документации
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import axios from "axios";
+import axios from 'axios';
 import Notiflix from 'notiflix';
-import ApiService from "./js/api-service";
+import ApiService from './js/api-service';
 
 const api = new ApiService();
 
 const refs = {
-    searchForm: document.querySelector("#search-form"),
-    gallery: document.querySelector(".gallery"),
-    loadMore: document.querySelector(".load-more")
-}
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+};
 
 const clearData = () => {
-    refs.gallery.innerHTML = '';
-  } 
+  refs.gallery.innerHTML = '';
+};
 
-const getData = (data) => {
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: false,
+  });
 
-    if (data.hits.length === 0) {
-        refs.loadMore.classList.add("visually-hidden");
-        clearData();
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        return;
+const getData = data => {
+  if (data.hits.length === 0) {
+    clearData();
+    refs.loadMore.classList.add('visually-hidden');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+    return;
+  }
+
+  if (api.page === Math.trunc(data.totalHits / data.hits.length)) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    refs.loadMore.classList.add('visually-hidden');
+    return;
+  }
+
+  if (data.hits.length > 0) {
+    if (api.page === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
     }
+    return data.hits;
+  }
+};
 
-    if (api.page === Math.trunc(data.totalHits / data.hits.length)) {
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-        refs.loadMore.classList.add("visually-hidden");
-        return;
-    }
-
-    if (data.hits.length > 0) {
-        if (api.page === 1) {
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        }
-        return data.hits;
-    }
-}
-
-const makeGalleryMarkup = (array) => {
-    const markup = array.map(item =>
+const makeGalleryMarkup = array => {
+  const markup = array
+    .map(
+      item =>
         `<div class="photo-card">
         <a href=${item.largeImageURL}>       
     <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
@@ -62,36 +69,37 @@ const makeGalleryMarkup = (array) => {
         <b>Downloads: </b>${item.downloads}
       </p>
     </div>
-    </div>`).join('');
-    
-  refs.gallery.insertAdjacentHTML("beforeend", markup);
-  refs.loadMore.classList.remove("visually-hidden");
+    </div>`,
+    )
+    .join('');
 
-  const lightbox = new SimpleLightbox('.gallery a');
-    }  
-    
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  refs.loadMore.classList.remove('visually-hidden');
 
-const showGallery = () => {
-        return api.fetchGallery(); 
-        }
+  lightbox.refresh();
+};
 
-const showNextGallery = () => {
-    api.incrementPage();
-    return api.fetchGallery(); 
-}
+const showGallery = async () => {
+  return api.fetchGallery();
+};
 
-refs.searchForm.addEventListener("submit", e => {
-    e.preventDefault();
-    clearData();
-    const searchQuery = e.currentTarget.elements.searchQuery.value;
-    api.newInput = searchQuery;
-    showGallery().then(getData).then(makeGalleryMarkup).catch(err => console.log(err));
-})
+const showNextGallery = async () => {
+  api.incrementPage();
+  return api.fetchGallery();
+};
 
-refs.loadMore.addEventListener("click", e => {
-    showNextGallery().then(getData).then(makeGalleryMarkup).catch(err => console.log(err));
+refs.searchForm.addEventListener('submit', async e => {
+  e.preventDefault();
+  clearData();
+  api.page = 1;
+  const searchQuery = e.currentTarget.elements.searchQuery.value;
+  api.newInput = searchQuery.trim();
+  const dataResult = await showGallery().catch(err => console.log(err));
+  makeGalleryMarkup(getData(dataResult));
+});
 
-})
-
-
-
+refs.loadMore.addEventListener('click', async e => {
+  const dataResult = await showNextGallery().catch(err => console.log(err));
+  makeGalleryMarkup(getData(dataResult));
+  // showNextGallery().then(getData).then(makeGalleryMarkup).catch(err => console.log(err));
+});
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,36 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default class ApiService {
-    #key = '23791107-11d3cac259792c6d6696bfd74';
-    BASE_URL = "https://pixabay.com/api/";
-      
-    constructor() {
-        this.page = 1;
-        this.searchItem = '';
-      }
-    
-    async fetchGallery () {
-        const queryParams = new URLSearchParams ({
-        key: this.#key,
-        q: this.searchItem,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: "true",
-        page: this.page,
-        per_page: 40
-        })
+  #key = '23791107-11d3cac259792c6d6696bfd74';
+  BASE_URL = 'https://pixabay.com/api/';
 
-        const res = await axios.get(`${this.BASE_URL}?${queryParams}`);
-            return res.data;
+  constructor() {
+    this.page = 1;
+    this.searchItem = '';
+  }
 
-        }
+  async fetchGallery() {
+    const queryParams = new URLSearchParams({
+      key: this.#key,
+      q: this.searchItem,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: this.page,
+      per_page: 40,
+    });
 
-    incrementPage() {
-        this.page += 1;
-    }
+    const res = await axios.get(`${this.BASE_URL}?${queryParams}`);
+    return res.data;
+  }
 
-    set newInput(input) {
-        this.searchItem = input;
-      }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    }
+  set newInput(input) {
+    this.searchItem = input;
+  }
+}
