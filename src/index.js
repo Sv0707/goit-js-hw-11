beefa8f0408fa28502e26ae3c9dd9924dd8e0812
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-15 17:14:01 +0300

    adding styles

--- src/index.js
+++ src/index.js
@@ -65,16 +65,16 @@ const makeGalleryMarkup = array => {
     <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
     <div class="info">
       <p class="info-item">
-        <b>Likes: </b>${item.likes}
+        <b>Likes</b>${item.likes}
       </p>
       <p class="info-item">
-        <b>Views: </b>${item.views}
+        <b>Views</b>${item.views}
       </p>
       <p class="info-item">
-        <b>Comments: </b>${item.comments}
+        <b>Comments</b>${item.comments}
       </p>
       <p class="info-item">
-        <b>Downloads: </b>${item.downloads}
+        <b>Downloads</b>${item.downloads}
       </p>
     </div>
     </div>`,
